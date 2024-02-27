393f03a193b95fa1548d6063238b93bb04ac2189
Maxim Reznik <reznikmm@gmail.com>
2024-02-27 14:47:20 +0200

    Try setup-gnatdoc

--- src/test.adb
+++ src/test.adb
@@ -1,6 +1,11 @@
 with Ada.Exceptions;
 with GNAT.Exception_Actions;
-procedure Test is
-begin
-   GNAT.Exception_Actions.Core_Dump (Ada.Exceptions.Null_Occurrence);
+
+package body Test is
+
+   procedure Core_Dump is
+   begin
+      GNAT.Exception_Actions.Core_Dump (Ada.Exceptions.Null_Occurrence);
+   end Core_Dump;
+
 end Test;
---
+++ src/test.ads
@@ -0,0 +1,7 @@
+--  Simple package for testing purposes.
+
+package Test is
+
+   procedure Core_Dump;
+   --  Generate a core dump
+end Test;
